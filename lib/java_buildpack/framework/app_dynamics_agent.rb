b5c383cce8f252e8eecb5638454bc96205943fe6
Jack Tams <jack.tams@sky.uk>
2017-07-04 19:50:56 +0100

    WIP

--- lib/java_buildpack/framework/app_dynamics_agent.rb
+++ lib/java_buildpack/framework/app_dynamics_agent.rb
@@ -46,7 +46,6 @@ module JavaBuildpack
         host_name java_opts, credentials
         port java_opts, credentials
         ssl_enabled java_opts, credentials
-        deployment_notifier credentials
 
         if !@application.services.find_service(PROXY_FILTER).nil?
           proxy_credentials = @application.services.find_service(PROXY_FILTER)['credentials']
@@ -54,6 +53,11 @@ module JavaBuildpack
           proxy_port java_opts, proxy_credentials
           proxy_user java_opts, proxy_credentials
           proxy_password_file java_opts, proxy_credentials
+        end
+
+        # Do Event Notification if we have API Credentials.
+        if !@application.services.find_service(API_FILTER).nil?
+          deployment_notifier @application.services.find_service(API_FILTER)['credentials'] credentials
         end
       end
 
@@ -66,37 +70,39 @@ module JavaBuildpack
 
       private
 
+      API_FILTER = /appd-api/
       FILTER = /app[-]?dynamics/
       PROXY_FILTER = /proxy/
 
       private_constant :FILTER
       private_constant :PROXY_FILTER
+      private_constant :API_FILTER
       
       # If api-user api-name are set on credenitals and appd-build set in env.
       # tell appd about this release.
-      def deployment_notifier(credentials)
+      def deployment_notifier(api_credentials, credentials)
         
-        if credentials['api-user'] and credentials ['api-password']
-            api_auth = credentials['api-auth']
+        if api_credentials['username'] and api_credentials['password']
             host_name = credentials['host-name']
-            tier_name = credentials['tier-name'] || @configuration['default_tier_name'] || @application.details['application_name']
-            summary = "Deployed Build ID: ${BUILD_ID}"
-            comment = "Comment"
-
-            events_uri = URI.parse("https://$host_name:$port/$app_name/events")
+            port = credentials['port']
+            app_name = credentials['application-name'] || @configuration['default_application_name'] || @application.details['application_name']
+            protocol = (credentials['ssl-enabled']) ? 'https' : 'http';
+            api_user = api_credentials['username']
+            account = credentials['account-name']
+            
+            events_uri = URI.parse("#{protocol}://#{host_name}:#{port}/#{app_name}/events")
             request = Net::HTTP::Post.new(events_uri.path)
-            request.basic_auth "credentials['api-user']@credentials['account-name']", credentials['api-password']
+            request.basic_auth "#{api_user}@#{account}", api_credentials['password']
             request.set_form_data({
               'eventtype' => 'APPLICATION_DEPLOYMENT',
-              'summary' => "Deployed: $app_name",
-              'severity' => "INFO"
+              'summary' => "Deploying: #{app_name}",
+              'severity' => 'INFO'
             })
             
             sock = Net::HTTP.new(events_uri.host, events_uri.port)
             sock.use_ssl = true
             res = sock.start { |http| http.request(req) }
             
-            # Debug LOG.
         end
       end
       
