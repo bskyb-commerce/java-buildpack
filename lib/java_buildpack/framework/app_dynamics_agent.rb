3d3299cb26cfd23b47e37274855bebe36e2fbbeb
Jack Tams <jack.tams@sky.uk>
2017-11-08 09:32:28 +0000

    WIP

--- lib/java_buildpack/framework/app_dynamics_agent.rb
+++ lib/java_buildpack/framework/app_dynamics_agent.rb
@@ -102,9 +102,11 @@ module JavaBuildpack
             
             events_uri = URI.parse("#{protocol}://#{host_name}:#{port}/controller/rest/applications/#{app_name}/events")
             
-            events_uri.query = URI.encode_www_form(['eventtype', 'APPLICATION_DEPLOYMENT'],
-              ['summary', "Deploying: #{app_name}"],
-              ['severity', 'INFO'])
+            events_uri.query = URI.encode_www_form(
+            'eventtype' => 'APPLICATION_DEPLOYMENT',
+            'summary' => "Deploying: #{app_name}",
+            'severity' => 'INFO'
+            )
               
             request = Net::HTTP::Post.new(events_uri.path)
             request.basic_auth "#{api_user}@#{account}", api_credentials['password']
