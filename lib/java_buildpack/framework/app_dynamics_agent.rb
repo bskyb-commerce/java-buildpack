a1ae5370f652cd74c16e549a8f98ddb6399c90df
Jack Tams <jack.tams@sky.uk>
2017-11-08 09:10:55 +0000

    Fix SSL

--- lib/java_buildpack/framework/app_dynamics_agent.rb
+++ lib/java_buildpack/framework/app_dynamics_agent.rb
@@ -113,8 +113,8 @@ module JavaBuildpack
             if !@application.services.find_service(PROXY_FILTER).nil?
               @logger.debug("Using Proxy to call AppD API.")
               proxy_credentials = @application.services.find_service(PROXY_FILTER)['credentials']
-              proxy = Net::HTTP::Proxy(proxy_credentials['host'], proxy_credentials['port'], proxy_credentials['user'], proxy_credentials['password'], :use_ssl => true)
-              res = proxy.start(events_uri.host, events_uri.port) do |http|
+              proxy = Net::HTTP::Proxy(proxy_credentials['host'], proxy_credentials['port'], proxy_credentials['user'], proxy_credentials['password'])
+              res = proxy.start(events_uri.host, events_uri.port, :use_ssl => events_uri.scheme == 'https') do |http|
                 http.request(request)
               end
               @logger.debug(res.body)
